3e6cb086b752fba155b74f8da6a72d0a51047f82
zeka321 <jkingzeka@gmail.com>
2024-08-19 00:02:47 +0800

    Add files via upload

--- script/gemini.js
+++ script/gemini.js
@@ -1,35 +1,36 @@
-module.exports = {
-  akane: {
-    name: "gemini",
-    permission: 0,
-    author: "Deku", //https://facebook.com/joshg101
-    description: "Talk to Gemini (conversational)",
-    prefix: false,
-    category: "AI",
-  },
-  start: async function ({ event, text, reply, react }) {
-    const axios = require("axios");
-    let prompt = text.join(" "),
-      uid = event.senderID,
-      url;
-    if (!prompt) return reply(`Please enter a prompt.`);
-    react('✨');
-    try {
-      const api = `https://gemini-api.replit.app`;
-      if (event.type == "message_reply"){
-        if (event.messageReply.attachments[0]?.type == "photo"){
+module.exports.config = {
+  name: "gemini",
+  role: 0,
+  credits: "Deku",
+  description: "Talk to Gemini (conversational)",
+  hasPrefix: false,
+  version: "5.6.7",
+  aliases: ["bard"],
+  usage: "gemini [prompt]"
+};
+
+module.exports.run = async function ({ api, event, args }) {
+  const axios = require("axios");
+  let prompt = encodeURIComponent(args.join(" ")),
+    uid = event.senderID,
+    url;
+  if (!prompt) return api.sendMessage(`Please enter a prompt.`, event.threadID);
+  api.sendTypingIndicator(event.threadID);
+  try {
+    const geminiApi = `https://ggwp-ifzt.onrender.com`;
+    if (event.type == "message_reply") {
+      if (event.messageReply.attachments[0]?.type == "photo") {
         url = encodeURIComponent(event.messageReply.attachments[0].url);
-        const res = (await axios.get(api + "/gemini?prompt="+prompt+"&url="+url+"&uid="+uid)).data;
-        return reply(res.gemini)
-        } else {
-          return reply('Please reply to an image.')
-        }
+        const res = (await axios.get(`${geminiApi}/gemini?prompt=${prompt}&url=${url}`)).data;
+        return api.sendMessage(res.gemini, event.threadID);
+      } else {
+        return api.sendMessage('Please reply to an image.', event.threadID);
       }
-      const rest = (await axios.get(api + "/gemini?prompt=" + prompt + "&uid=" + uid)).data;
-      return reply(rest.gemini)
-    } catch (e) {
-      console.log(e);
-      return reply(e.message);
-    } //end of catch
-  }, // end of start
-}; // end of exports
+    }
+    const response = (await axios.get(`${geminiApi}/gemini?prompt=${prompt}`)).data;
+    return api.sendMessage(response.gemini, event.threadID);
+  } catch (error) {
+    console.error(error);
+    return api.sendMessage('Error skills issue', event.threadID);
+  }
+};
