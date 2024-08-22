3db32b1bda7f45884d9126750a1f58396b63bf9d
zeka321 <jkingzeka@gmail.com>
2024-08-22 19:38:06 +0800

    Add files via upload

--- script/ai.js
+++ script/ai.js
@@ -1,33 +1,51 @@
 const axios = require('axios');
+
 module.exports.config = {
   name: 'ai',
   version: '1.0.0',
   role: 0,
   hasPrefix: false,
-  aliases: ['gpt', 'openai'],
+  aliases: [],
   description: "An AI command powered by GPT-4",
-  usage: "Ai [promot]",
+  usages: "ai [prompt]",
   credits: 'Developer',
-  cooldown: 3,
+  cooldowns: 3,
+  dependencies: {
+    "axios": ""
+  }
 };
-module.exports.run = async function({
-  api,
-  event,
-  args
-}) {
+
+module.exports["run"] = async function({ api, event, args }) {
   const input = args.join(' ');
+
   if (!input) {
     api.sendMessage(`Please provide a question or statement after 'ai'. For example: 'ai What is the capital of France?'`, event.threadID, event.messageID);
     return;
   }
-  api.sendMessage(`🔍 "${input}"`, event.threadID, event.messageID);
+  
+  if (input === "clear") {
+    try {
+      await axios.post('https://satomoigpt.onrender.com/clear', { id: event.senderID });
+      return api.sendMessage("Chat history has been cleared.", event.threadID, event.messageID);
+    } catch {
+      return api.sendMessage('An error occurred while clearing the chat history.', event.threadID, event.messageID);
+    }
+  }
+
+  api.sendMessage(`🔍 "${input}"...`, event.threadID, event.messageID);
+  
   try {
-    const {
-      data
-    } = await axios.get(`https://markdevs-last-api-2epw.onrender.com/api/v2/gpt4?query=${encodeURIComponent(input)}`);
-    const respond = data.respond;
-    api.sendMessage(respond, event.threadID, event.messageID);
-  } catch (error) {
+    const url = event.type === "message_reply" && event.messageReply.attachments[0]?.type === "photo"
+      ? { link: event.messageReply.attachments[0].url }
+      : {};
+
+    const { data } = await axios.post('https://satomoigpt.onrender.com/chat', {
+      prompt: input,
+      customId: event.senderID,
+      ...url
+    });
+    api.sendMessage(`卐 | 𝗚𝗣𝗧-𝟰 (𝗔𝘀𝘀𝗶𝘀𝘁𝗮𝗻𝘁)\n━━━━━━━━━━━━━━━━━━\n${data.message}\n━━━━━━━━━━━━━━━━━━\n卐 𝙾𝚠𝚗𝚎𝚛 : 𝙷𝚘𝚖𝚎𝚛 𝚁𝚎𝚋𝚊𝚝𝚒𝚜`, event.threadID, event.messageID);
+  } catch {
     api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
   }
 };
