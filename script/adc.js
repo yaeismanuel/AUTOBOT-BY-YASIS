cf7eeffe4717a51c4cbf1901235e1fd8374b0327
yaeismanuel <rickyportu@gmail.com>
2025-02-06 00:54:18 +0800

    Update adc.js

--- script/adc.js
+++ script/adc.js
@@ -1,7 +1,7 @@
 module.exports.config = {
   name: "adc",
   version: "1.0.0",
-  role: 3,
+  role: 0,
   hasPrefix: true,
   usage: '[reply or text]',
   description: 'Apply code from buildtooldev and pastebin',
